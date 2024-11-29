ae38cec5eb1fb786f25c668556f988903bf1b753
Alex <423745+alexgenovese@users.noreply.github.com>
2024-11-29 16:09:42 +0100

    small update

--- app/page.tsx
+++ app/page.tsx
@@ -15,12 +15,12 @@ export default function Home() {
           <div className="page-padding">
             <div className="container-normal">
               <div className="banner_wrapper">
-                <h1 className="text-align-center max-width-915"> Increase your team productivity integrating Generative AI</h1>
-                <p className="text-align-center margin-top-30 color-5e5e5e max-width-730">Scale up your productivity team without expanding your team with complexy hiring or time wasting to find out the perfect freelance available.</p>
+                <h1 className="text-align-center max-width-915"> Increase your sales with Artificial Intelligence Marketing</h1>
+                <p className="text-align-center margin-top-30 color-5e5e5e max-width-730">Scale up your sales without complex marketing stuff</p>
                 <div className="button_wrapper center-aligned"><a href="/call" className="primary_button width-320 w-button">Book a Discovery Call</a><a href="/showcase" className="primary_button is-secondary width-320 w-button">See Previous Work</a></div>
                 <div className="features-check_wrapper">
                   <div className="item-list_check"><Image src="images/check-icon.svg" loading="lazy" alt=""  width={32} height={32} className="list-check_icon" />
-                    <div className="list-banner">Unlimited generations &amp; edits requests</div>
+                    <div className="list-banner">x3 minimum increase</div>
                   </div>
                   <div className="item-list_check"><Image src="images/check-icon.svg" loading="lazy" alt="" width={32} height={32} className="list-check_icon" />
                     <div className="list-banner">Monthly flat-rate</div>
